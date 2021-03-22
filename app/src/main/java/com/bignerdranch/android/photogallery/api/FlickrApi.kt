bb25b21be5a90657e23f2cc32612cbaa7e7220fa
anthonyz713 <78340967+anthonyz713@users.noreply.github.com>
2021-03-21 23:00:52 -0700

    Done with Ch 24

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=7d96303709bae546e0c8f16621734541" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+
+}
